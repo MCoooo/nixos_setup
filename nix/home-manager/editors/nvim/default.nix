9445c4c983e1750f2ec79b2fa3d399a92eadebd7
mcoooo <martin.coomber@hotmail.com>
2024-01-15 11:40:47 +0000

    Remove lua mappings top save fixing error

--- nix/home-manager/editors/nvim/default.nix
+++ nix/home-manager/editors/nvim/default.nix
@@ -11,90 +11,95 @@
 
     plugins = {
       telescope = {
-	enable = true;
-	keymapsSilent = true;
-	keymaps = {
-	  "<C-p>" = {
-	    action = "git_files";
-	    desc = "Telescope Git Files";
-	  };
-	  "<leader>fd" = {
-	    action = "fd";
-	    desc = "Telescope fd";
-	  };
-	  "<leader>fg" = "live_grep";
-	  "<leader>ff" = "find_files";
-	  "<leader>." = "oldfiles";
-	  "<leader>," = "buffers";
-	};
-	defaults = {
-	  border = true;
-	  layout_strategy = "horizontal";
-	  layout_config = {
-	    anchor = "center";
-	    height = 0.75;
-	    width = 0.5;
-	    prompt_position = "bottom";
-	  };
-	};
+        enable = true;
+        keymapsSilent = true;
+        keymaps = {
+          "<C-p>" = {
+            action = "git_files";
+            desc = "Telescope Git Files";
+          };
+          "<leader>fd" = {
+            action = "fd";
+            desc = "Telescope fd";
+          };
+          "<leader>fg" = "live_grep";
+          "<leader>ff" = "find_files";
+          "<leader>." = "oldfiles";
+          "<leader>," = "buffers";
+        };
+        defaults = {
+          border = true;
+          layout_strategy = "horizontal";
+          layout_config = {
+            anchor = "center";
+            height = 0.75;
+            width = 0.5;
+            prompt_position = "bottom";
+          };
+        };
       };
       nvim-cmp = {
-	enable = true;
-	autoEnableSources = true;
-	sources = [
-	  {name = "nvim_lsp";}
-	  {name = "path";}
-	  {name = "buffer";}
-	  {name = "luasnip";}
-	];
-
-	mapping = {
-	  "<CR>" = "cmp.mapping.confirm({ select = true })";
-	  "<Tab>" = {
-	    action = ''
-	      function(fallback)
-		if cmp.visible() then
-		  cmp.select_next_item()
-		elseif luasnip.expandable() then
-		  luasnip.expand()
-		elseif luasnip.expand_or_jumpable() then
-		  luasnip.expand_or_jump()
-		elseif check_backspace() then
-		  fallback()
-		else
-		  fallback()
-		end
-	      end
-	    '';
-	    modes = [ "i" "s" ];
-	  };
-	};
+        enable = true;
+        autoEnableSources = true;
+        sources = [
+          {name = "nvim_lsp";}
+          {name = "path";}
+          {name = "buffer";}
+          {name = "luasnip";}
+        ];
       };
-
       lsp = {
         enable = true;
         servers = {
           tsserver = {
-	    enable = true;
-	  };
-	  lua-ls = {
-	    enable = true;
-	    settings.telemetry.enable = false;
-	  };
-	  rust-analyzer = {
-	    enable = true;
-	    installCargo = true;
-	    installRustc = true;
-	  };
+            enable = true;
+          };
+          lua-ls = {
+            enable = true;
+            settings.telemetry.enable = false;
+          };
+          rust-analyzer = {
+            enable = true;
+            installCargo = true;
+            installRustc = true;
+          };
         };
+        keymaps = {
+          diagnostic = {
+            "]d" = "goto_next";
+            "[d" = "goto_prev";
+          };
+          lspBuf = {
+            K = "hover";
+            gD = "declaration";
+            gr = "references";
+            gd = "definition";
+            gi = "implementation";
+            gt = "type_definition";
+            "<leader>cr" = { action = "rename"; desc = "Rename"; };
+            "<leader>ca" = { action = "code_action"; desc = "Show Code Actions"; };
+          };
+        }; 
       };
     };
+    extraConfigLua =
+      # lua
+      ''
+        vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+        	border = "rounded",
+        })
 
+        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        	callback = function()
+        		require("lint").try_lint()
+        	end,
+        })
+      '';
     extraPlugins = with pkgs.vimPlugins; [
-	{
-	  plugin = comment-nvim;
-	  config = "lua require(\"Comment\").setup()";
-	}
+      {
+        plugin = comment-nvim;
+        config = "lua require(\"Comment\").setup()";
+      }
     ];
   };
 }
